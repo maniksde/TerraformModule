0446cbd6624beb108bf997bc2b5d264f6c09e5f3
Manik Chakraborty <35147661+maniksde@users.noreply.github.com>
2023-03-02 15:23:10 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  type  = string
+}
+
+variable "project" {
+  type = string
+}
+variable "vpc_network" {
+  type = string
+}
+variable "auto_create_subnetworks" {
+  type = bool
+}
+
+
+
