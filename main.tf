a9bcd29fd7fbf33d24ef7f2921f13ea90a505dc9
Manik Chakraborty <35147661+maniksde@users.noreply.github.com>
2023-03-03 19:02:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.55.0"
+    }
+  }
+}
+
+
 resource "google_compute_network" "vpc_network" {
   name = var.vpc_network
   auto_create_subnetworks = var.auto_create_subnetworks
