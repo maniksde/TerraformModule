383d2fa976c70065c11ce56fd46c8ad73f8acd55
Manik Chakraborty <35147661+maniksde@users.noreply.github.com>
2023-03-06 01:45:22 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google"{
-    credentials = file("/home/manikchakrabortysde/manik-key.json")
+    credentials = file("/home/manikchakrabortysde/o-media-2-1caeacc0a56f.json")
     project = "o-media-2"
     region = "us-central1"
 }
